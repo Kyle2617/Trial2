d110049e1359604523c9d8c0b824db631b77d7f4
achmet <gigsakos@gmail.com>
2024-07-10 22:22:23 +0300

    first touch

--- server/index.js
+++ server/index.js
@@ -1,28 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const mealRoutes = require('./routes/meals');
-const userRoutes = require('./routes/users');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const mealRoutes = require("./routes/meals");
+const userRoutes = require("./routes/users");
+const connection = require("./Config/Connection");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected...'))
-.catch(err => console.log(err));
-
-app.use('/meals', mealRoutes);
-app.use('/users', userRoutes);
+app.use("/meals", mealRoutes);
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
